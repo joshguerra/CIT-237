a7b99f5f85f93274b301133661fb0a2d6a1f5d7d
Joshua Guerra <joshua@allianceconsults.net>
2016-01-31 01:54:05 -0500

    added printArray function and completed code for design

--- Labs/Lab1A-JGuerra.cpp
+++ Labs/Lab1A-JGuerra.cpp
@@ -44,7 +44,13 @@ Wash. Rinse. Repeat starting at print statements for each different variable and
 
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+//  Pre Condition:  0 < n <= to the LOGICAL size of a[]    
+//					os must be a valid open output stream
+// Post Condition:  Prints the first n elements of a[] to os
+void printStringArr(const string a[], int n, ostream &os = cout);
 
 int main() {
 	char myChar = 'j';
@@ -64,11 +70,22 @@ int main() {
 
 	float myFloat = 1.2f;
 	float *myFloatPointer = &myFloat;
+	
+
+	int const SIZE_TITLE = 3;
+	string titleArr[SIZE_TITLE] = {
+		"\t\t                     Lab 1A                     ",
+		"\t\t                  by J. Guerra                  ",
+		"\t\t        Modifying variables via pointers        "
+	};
+
+	printStringArr(titleArr, SIZE_TITLE);
+	cout << "\n\n";
 
 	cout
 		<< "myChar: " << myChar << endl
-		<< "address of myChar: " << &myChar << endl
-		<< "myCharPointer: " << myCharPointer << endl
+		<< "address of myChar: " << int(&myChar) << endl
+		<< "myCharPointer: " << int(myCharPointer) << endl
 		<< "value myCharPointer is looking at: " << *myCharPointer
 		<< endl << endl;
 
@@ -76,13 +93,108 @@ int main() {
 
 	cout
 		<< "myChar: " << myChar << endl
-		<< "address of myChar: " << &myChar << endl
-		<< "myCharPointer: " << myCharPointer << endl
+		<< "address of myChar: " << int(&myChar) << endl
+		<< "myCharPointer: " << int(myCharPointer) << endl
 		<< "value myCharPointer is looking at: " << *myCharPointer
+		<< endl << endl;
+
+	cout << "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ " << endl << endl;
+
+	cout
+		<< "myInt: " << myInt << endl
+		<< "address of myInt: " << &myInt << endl
+		<< "myIntPointer: " << myIntPointer << endl
+		<< "value myIntPointer is looking at: " << *myIntPointer
+		<< endl << endl;
+
+	*myIntPointer = 17;
+
+	cout
+		<< "myInt: " << myInt << endl
+		<< "address of myInt: " << &myInt << endl
+		<< "myIntPointer: " << myIntPointer << endl
+		<< "value myIntPointer is looking at: " << *myIntPointer
+		<< endl << endl;
+
+	cout << "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ " << endl << endl;
+
+	cout
+		<< "myShort: " << myShort << endl
+		<< "address of myShort: " << &myShort << endl
+		<< "myShortPointer: " << myShortPointer << endl
+		<< "value myShortPointer is looking at: " << *myShortPointer
+		<< endl << endl;
+
+	*myShortPointer = 3;
+
+	cout
+		<< "myShort: " << myShort << endl
+		<< "address of myShort: " << &myShort << endl
+		<< "myShortPointer: " << myShortPointer << endl
+		<< "value myShortPointer is looking at: " << *myShortPointer
+		<< endl << endl;
+
+	cout << "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ " << endl << endl;
+
+	cout
+		<< "myLong: " << myLong << endl
+		<< "address of myLong: " << &myLong << endl
+		<< "myLongPointer: " << myLongPointer << endl
+		<< "value myLongPointer is looking at: " << *myLongPointer
+		<< endl << endl;
+
+	*myLongPointer = 987654321;
+
+	cout
+		<< "myLong: " << myLong << endl
+		<< "address of myLong: " << &myLong << endl
+		<< "myLongPointer: " << myLongPointer << endl
+		<< "value myLongPointer is looking at: " << *myLongPointer
+		<< endl << endl;
+
+	cout << "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ " << endl << endl;
+
+	cout
+		<< "myDouble: " << myDouble << endl
+		<< "address of myDouble: " << &myDouble << endl
+		<< "myDoublePointer: " << myDoublePointer << endl
+		<< "value myDoublePointer is looking at: " << *myDoublePointer
+		<< endl << endl;
+
+	*myDoublePointer = 67.89;
+
+	cout
+		<< "myDouble: " << myDouble << endl
+		<< "address of myDouble: " << &myDouble << endl
+		<< "myDoublePointer: " << myDoublePointer << endl
+		<< "value myDoublePointer is looking at: " << *myDoublePointer
+		<< endl << endl;
+
+	cout << "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ " << endl << endl;
+
+	cout
+		<< "myFloat: " << myFloat << endl
+		<< "address of myFloat: " << &myFloat << endl
+		<< "myFloatPointer: " << myFloatPointer << endl
+		<< "value myFloatPointer is looking at: " << *myFloatPointer
+		<< endl << endl;
+
+	*myFloatPointer = 3.4f;
+
+	cout
+		<< "myFloat: " << myFloat << endl
+		<< "address of myFloat: " << &myFloat << endl
+		<< "myFloatPointer: " << myFloatPointer << endl
+		<< "value myFloatPointer is looking at: " << *myFloatPointer
 		<< endl << endl;
 
 	cout << "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ " << endl << endl;
 
 	system("pause");
 	return 0;
+}
+
+void printStringArr(const string a[], int n, ostream &os) {
+	for (int i = 0; i < n; i++)
+		os << "\n" << a[i];
 }
